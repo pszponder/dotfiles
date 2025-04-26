fe21e3c10de996a3ac46434d48278a0c13d8f900
pszponder <14116413+pszponder@users.noreply.github.com>
2025-04-25 20:59:08 -0600

    refactor: change shell config files to template files

--- home/dot_config/fish/config.fish
+++
@@ -1,9 +0,0 @@
-# ~/.config/fish/config.fish
-
-# Add directories to path
-set -U fish_user_paths $fish_user_paths ~/bin ~/scripts /usr/local/bin
-
-# Homebrew (Linux) initialization
-if test -f /home/linuxbrew/.linuxbrew/bin/brew
-  eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-end

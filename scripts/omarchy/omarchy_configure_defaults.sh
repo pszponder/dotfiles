bd44403460ee483047ba5eb7db6163ff9eeba032
pszponder <14116413+pszponder@users.noreply.github.com>
2026-09-13 11:13:45 -0600

    feat: set default editor to neovim

--- scripts/omarchy/omarchy_configure_defaults.sh
+++ scripts/omarchy/omarchy_configure_defaults.sh
@@ -25,8 +25,8 @@ omarchy font set "CaskaydiaCove Nerd Font"
 log_info "Setting the default terminal to Ghostty..."
 omarchy default terminal ghostty
 
-log_info "Setting the default editor to VS Code..."
-omarchy default editor code
+log_info "Setting the default editor to Neovim..."
+omarchy default editor nvim
 
 log_info "Setting the default browser to Brave Origin..."
 omarchy default browser brave-origin
